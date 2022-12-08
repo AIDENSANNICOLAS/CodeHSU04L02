e43474e66087a4cb39ba4cd0c62d1e1e86580762
AIDENSANNICOLAS <sannicoa@wlhs.wlwv.k12.or.us>
2022-12-08 09:41:57 -0800

    uh oph

--- CodeHSU04L02/src/MoreLoopFun.java
+++ CodeHSU04L02/src/MoreLoopFun.java
@@ -7,17 +7,31 @@ public class MoreLoopFun {
          */
 
         //a for loop that counts by ones from 1 to 5
+        for (int start = 1; start < 6; start++) {
+            System.out.println(start);
+        }
 
 
-        //a for loop that prints the powers of two up to 100
+        //a for loop that prints the integer powers of two up to 100
         //can you have an infinite for loop?
-
+        for (int start2 = 1; start2 <= 100; start *= 2){
+        System.out.println(start2 +" ");
+    }
 
         //write a for loop that counts by 4s from 5 up to 50
+        for(int start3 = 5; start3 <= 50; start3 += 4){
+            System.out.print(start3 + " ");
+        }
 
 
         //write a for loop that sums the numbers from 1 to 10
-        int total = 0;
+        for(int sum = 1; sum <= 11; sum += sum)  {
+            System.out.println(sum);
+
+        }
+
+
+        /*int total = 0;
 
         ////////////////////////////////////////////////////
         //for loops compared to while loops////////////////
@@ -33,7 +47,12 @@ public class MoreLoopFun {
             System.out.println(num);
             num++;
         }
+        */
+
+
         System.out.println("Finished!");
+
+
 
     }
 }
